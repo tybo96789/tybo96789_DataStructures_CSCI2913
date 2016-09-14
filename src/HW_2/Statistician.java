f64626a4c8030d37248cbe7ab89a4df29e06066c
Tyler A <tyler.atiburcio@gmail.com>
2016-09-13 22:38:00 -1000

    HW-2 Staticitian Program GM
    Comments and boundary cases: Done

--- src/HW_2/Statistician.java
+++ src/HW_2/Statistician.java
@@ -1,18 +1,24 @@
 package HW_2;
 
-/**
+/**Statistician; Various methods to obtain information about the sequence
  *
  * @author Tyler Atiburcio
  */
 public class Statistician {
     
-    private double sum = 0, lastEnter = Double.NaN, largest = Double.NaN, smallest = Double.NaN;
+    /**Declaration and initialization of essential variables to their specified failsafe values:
+     * Sum, Mean, Largest, Smallest, Last Entered, and Length
+     */
+    private double sum = 0.0, lastEnter = Double.NaN, largest = Double.NaN, smallest = Double.NaN;
     private int length = 0;
-
-    public Statistician() {
-        
-    }
     
+    /**Takes in a double and updates variables accordingly
+     * 
+     * Precondition: val is a double or any Real number
+     * Postcondition: Length and sum  and other related variables are updated accordingly
+     * 
+     * @param val A real number
+     */
     public void nextNumber(double val)
     {
         largest = length == 0 || val > largest ? val : largest;
@@ -21,34 +27,60 @@ public class Statistician {
         sum+= val;
         length++;
     }
-
+    
+    /**Postcondition: Returns the Sum of the sequence
+     * if length is 0, default should return 0
+     * 
+     * @return The sum of the sequence
+     */
     public double getSum() {
         return sum;
     }
-
+    
+    /**PostCondition: Returns the last number entered in the sequence
+     * if length is 0, default should return Double.NaN
+     * 
+     * @return Returns the last number entered in the sequence
+     */
     public double getLastEnter() {
         return lastEnter;
     }
-
+    
+    /**PostCondition: Returns the Largest number in the sequence
+     * if length is 0, default should return Double.NaN
+     * 
+     * @return Returns the largest number in the sequence
+     */
     public double getLargest() {
         return largest;
     }
-
+    
+    /**PostCondition: Returns the smallest number in the sequence
+     * if length is 0, default should return Double.NaN
+     * 
+     * @return Returns the smallest number in the sequence
+     */
     public double getSmallest() {
         return smallest;
     }
-
+    
+    /**PostCondition: Return the length of the sequence
+     * if length is 0, default should return 0
+     * 
+     * @return The amount of numbers that has been entered
+     */
     public int getLength() {
         return length;
     }
     
+    /**PostCondition: Returns the mean of the sequence,
+     * if length is 0, default should return Double.NaN
+     * 
+     * @return Mean of the sequence
+     */
     public double getMean()
     {
         return this.length == 0 ? 0 : this.getSum()/this.getLength();
     }
-    
-    
-    
-    
 
 }
--- src/HW_2/StatisticianTest.java
+++ src/HW_2/StatisticianTest.java
@@ -34,6 +34,38 @@ public class StatisticianTest {
         System.out.println("Largest: "+s.getLargest());
         System.out.println("Smallest: "+s.getSmallest());
         
+        System.out.println("\n\n\n");
+        
+        //Boundary Case 
+        //Sum = 0
+        System.out.println("Boundary Case: Sum equals 0");
+        s = new Statistician();
+        s.nextNumber(-1.0);
+        s.nextNumber(0);
+        s.nextNumber(1.0);
+        System.out.println("Len:" + s.getLength());
+        System.out.println("Avg: "+s.getMean());
+        System.out.println("Sum: "+ s.getSum());
+        System.out.println("Last Enter: "+s.getLastEnter());
+        System.out.println("Largest: "+s.getLargest());
+        System.out.println("Smallest: "+s.getSmallest());
+        
+        System.out.println("\n\n\n");
+        
+        //Boundary Case 
+        //Repeated numbers
+        System.out.println("Boundary Case: Repeated Numbers");
+        s = new Statistician();
+        s.nextNumber(1);
+        s.nextNumber(1);
+        s.nextNumber(1);
+        System.out.println("Len:" + s.getLength());
+        System.out.println("Avg: "+s.getMean());
+        System.out.println("Sum: "+ s.getSum());
+        System.out.println("Last Enter: "+s.getLastEnter());
+        System.out.println("Largest: "+s.getLargest());
+        System.out.println("Smallest: "+s.getSmallest());
+        
     }
 
 }
