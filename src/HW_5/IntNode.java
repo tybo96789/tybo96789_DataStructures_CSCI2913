397fa3cd8c538dd2b82b5d8ee3e53230f0263866
Tyler A <tyler.atiburcio@gmail.com>
2016-10-06 00:08:26 -1000

    HW5 GM
    Comments Added

--- src/HW_5/IntNode.java
+++ src/HW_5/IntNode.java
@@ -11,25 +11,25 @@ public class IntNode {
     private int data;
     private IntNode next;
     
-    /**
+    /**Creates a new Node
      * 
-     * @param data
-     * @param next 
+     * @param data An Integer
+     * @param next The next node in the list
      */
     public IntNode(int data, IntNode next) {
         this.data = data;
         this.next = next;
     }
     
-    /**
+    /**Returns the data in the node
      * 
-     * @return 
+     * @return data An Integer
      */
     public int getData() {
         return data;
     }
     
-    /**
+    /**Returns the next node in the list
      * 
      * @return the nextNode in the list
      */
@@ -37,7 +37,7 @@ public class IntNode {
         return next;
     }
     
-    /**
+    /**Sets the data (Integer) to the node
      * 
      * @param data That is contained in the node
      */
@@ -45,15 +45,16 @@ public class IntNode {
         this.data = data;
     }
     
-    /**
+    /**Sets the pointer to the given passed argument of the next
      * 
-     * @param next The next Object Node that it should point to next
+     * @param next The next IntNode that it should point to next
      */
     public void setNext(IntNode next) {
         this.next = next;
     }
     
-    /**
+    /**Reverses the order of the list
+     * The head becomes the tail, the tail becomes the head
      * 
      * @param head
      * @return The head of the list
@@ -67,11 +68,11 @@ public class IntNode {
             IntNode prev = null, temp = null;
             while(head != null)
             {
-                temp = head.getNext();
-                head.setNext(prev);
-                prev = head;
-                if(temp == null) return head;
-                head = temp;
+                temp = head.getNext();  //Get the next IntNode
+                head.setNext(prev);     //Set the current head node to the previous node
+                prev = head;            //set the previous node to the current head
+                if(temp == null) return head;   //if the next node is null, meaning the ending of the list return
+                head = temp;            //if the next node in the list is not null set the head to the next node in the list
             }
             return head;
         }
