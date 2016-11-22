2ff6a0cde7d74f9f2584908bc5b917309ea02efb
Tyler A <tyler.atiburcio@gmail.com>
2016-11-22 00:00:17 -1000

    Hw7 Beta

--- src/HW_7/PriorityQueue.java
+++ src/HW_7/PriorityQueue.java
@@ -1,17 +1,19 @@
 package HW_7;
 
-/**
+/**Implementation of priority queue ADT via heap
  *
  * @author Tyler Atiburcio
  */
 public class PriorityQueue<T> {
     
-    public int items;
-    public Object[] data;
-    public int[] priority;
-    public int[] entered;
+    private int items;                   //Items currently in tree
+    private Object[] data;               //Data array of nodes
+    private int[] priority;              //Array to hold priority data
+    private int[] entered;               //Timestamp of node 
     
-    
+    /**Makes a Priority Queue with the starting size of 10
+     * 
+     */
     public PriorityQueue()
     {
         this.entered = new int[10];
@@ -19,10 +21,17 @@ public class PriorityQueue<T> {
         this.priority = new int[10];
     }
     
+    /**Add Node to the heap given the priority
+     * 
+     * @param data The data to be associated with the node
+     * @param priority The priority the node should have in the queue
+     */
     public void add(T data, int priority)
-    {
+    {   
+        //If inital data array is empty
         if(this.data == null)
             this.data =  new Object[10];
+        //Double the size of all arrays if their are more items than array space
         if(items == this.data.length)
         {
             Object[] temp = new Object[this.data.length*2];
@@ -43,6 +52,11 @@ public class PriorityQueue<T> {
         this.reHeapUpwards(items-1);
     }
     
+    /**Move the new up the tree to keep the properties of b-tree
+     * 
+     * @param loc The location to start comparing 
+     * @return new location to start comparing priorities
+     */
     private int reHeapUpwards(int loc)
     {
         if(loc == 0 || (this.priority[(loc-1)/2] > this.priority[loc])) return loc;
@@ -50,7 +64,13 @@ public class PriorityQueue<T> {
         return (loc-1)/2;
     }
     
-    public void swap(int locA, int locB)
+    /**Swap the data between the two indexies,
+     * parallel to the 3 data arrays
+     * 
+     * @param locA Index of one Node
+     * @param locB Index of another Node
+     */
+    private void swap(int locA, int locB)
     {
         Object temp = this.data[locA];
         this.data[locA] = this.data[locB];
@@ -65,13 +85,21 @@ public class PriorityQueue<T> {
         this.entered[locB] = temp1;
     }
     
-    
+    /**Remove node from the queue (Root of tree)
+     * 
+     * @return Data associated with that node
+     */
     public T remove()
     {
+        //If heap is empty, return null
+        if(this.items == 0) return null;
+        //Move root to end
         swap(0,--items);
+        //Begin reheapification downward by swaping the root with its children until it's a b-tree again
         int loc = 1;
         while(this.priority[loc] > this.priority[(loc-1)/2])
         {
+            //If both children have the same priority, compare them by when they were entered into the array
             if(this.priority[loc*2+1] == this.priority[(loc+1)*2])
             {
                 if(this.entered[(loc*2+1)] < this.entered[(loc+1)*2])
@@ -85,11 +113,13 @@ public class PriorityQueue<T> {
                     loc = (loc+1)*2;
                 }
             }
+            //Check left child
             else if(this.priority[loc*2+1] > this.priority[loc])
             {
                 swap((loc*2+1),loc);
                 loc = loc*2+1;
             }
+            //Check right child
             else if(this.priority[(loc+1)*2] > this.priority[loc])
             {
                 swap(loc,(loc+1)*2);
@@ -98,6 +128,22 @@ public class PriorityQueue<T> {
             
         }
         return (T) this.data[items];
+    }
+    
+    public int getItems() {
+        return items;
+    }
+
+    public Object[] getData() {
+        return data;
+    }
+
+    public int[] getPriority() {
+        return priority;
+    }
+
+    public int[] getEntered() {
+        return entered;
     }
     
     
--- src/HW_7/PriorityQueueTest.java
+++ src/HW_7/PriorityQueueTest.java
@@ -8,19 +8,21 @@ public class PriorityQueueTest {
 
     public static void main(String[] args) {
         PriorityQueue pq = new PriorityQueue();
+        
+        //Standard Case
         pq.add(1, 2);
         pq.add(2, 3);
         pq.add(3, 1);
         pq.add(4, 3);
-        pq.add(5,3);
-        for (int i = 0; i < pq.items; i++) {
-            System.out.println(pq.data[i] + " " + pq.priority[i] + " " + pq.entered[i]);
+        pq.add(5, 3);
+        for (int i = 0; i < pq.getItems(); i++) {
+            System.out.println(pq.getData()[i] + " " + pq.getPriority()[i] + " " + pq.getEntered()[i]);
         }
         System.out.println("");
         System.out.println(pq.remove());
         System.out.println("");
-        for (int i = 0; i < pq.items; i++) {
-            System.out.println(pq.data[i] + " " + pq.priority[i] + " " + pq.entered[i]);
+        for (int i = 0; i < pq.getItems(); i++) {
+            System.out.println(pq.getData()[i] + " " + pq.getPriority()[i] + " " + pq.getEntered()[i]);
         }
 
     }
